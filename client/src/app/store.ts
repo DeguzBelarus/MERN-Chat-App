175a3adc4322c25845b98f7972b6e3c40745d19f
Anton <deguz@mail.ru>
2022-01-18 18:38:49 +0300

    delete redux features demo

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import userReducer from "../app/userSlice"
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     user: userReducer
   },
 });
