0975a5621ad33b4603eb58b7a83d29bec18f84c0
Anton <deguz@mail.ru>
2022-02-06 14:56:04 +0300

    production build

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = config.get("port") || 5000;
 
@@ -10,6 +11,13 @@ const io = require("socket.io")(server);
 
 app.use(express.json({ extended: true }));
 app.use("/api/authorization", require("./routes/authorization.router"));
+
+if ((process.env.NODE_ENV = production)) {
+  app.use("/", express.static(path.join(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 let usersInRoom = [];
 
