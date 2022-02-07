6ffeea5d094078b3315adc53c126b133af68fd5a
Anton <deguz@mail.ru>
2022-02-07 14:03:43 +0300

    some server logs removed

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ if (process.env.NODE_ENV === "production") {
 let usersInRoom = [];
 
 io.on("connection", (socket) => {
-  console.log(`user connected on socket id: ${socket.id}`);
-
   socket.on("user connected", (data) => {
     console.log(`connection data: ${data.nickname}`);
     const connectedUser = data.nickname;
@@ -49,7 +47,6 @@ io.on("connection", (socket) => {
     });
 
     socket.on("user send message", (nickname, message) => {
-      console.log(nickname, message);
       socket.emit("message from user", nickname, message);
       socket.broadcast.emit("message from user", nickname, message);
     });
@@ -58,21 +55,12 @@ io.on("connection", (socket) => {
       const targetUser = usersInRoom.filter((element) => {
         return element[0] === nickname;
       });
-      console.log("Targeted user: ", targetUser);
-
       socket.emit("getting private user data", targetUser);
     });
 
     socket.on(
       "user send private message",
       (nickname, privatemessage, privateUserNick, privateUserSocket) => {
-        console.log(
-          nickname,
-          privatemessage,
-          privateUserNick,
-          privateUserSocket
-        );
-
         socket.emit(
           "private message notification",
           privateUserNick,
