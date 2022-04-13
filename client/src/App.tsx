2a3f94d1b0c06915c62ba81836f4ebf63ad594ee
Anton <deguz@mail.ru>
2022-04-13 13:11:08 +0300

    the code is made prettier in the App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,10 +8,13 @@ const App: FC<Props> = ({ socket }) => {
    const routes = useRoutes(socket)
 
    useEffect(() => {
+      //== notification of connection establishment
       socket.on("connect", () => {
          console.log("websocket connection has been established...");
       })
+      //== notification of connection establishment
    }, [])
+
    return (
       <>
          {routes}
